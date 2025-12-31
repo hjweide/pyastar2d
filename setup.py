bc5dc9ac49c4a0b082e42ecdb5e73d222e8dba73
Jason Parham <jason.parham@kitware.com>
2025-12-31 15:01:20 -0800

    Modernize setuptools, add pre-commit for code formatting and linting

--- setup.py
+++ setup.py
@@ -1,44 +1,55 @@
-import setuptools
-from distutils.core import Extension
-from setuptools import dist
-dist.Distribution().fetch_build_eggs(["numpy"])
-import numpy
+import pathlib
 
+from setuptools import Extension, find_packages, setup
+
+# Use pathlib for paths
+here = pathlib.Path(__file__).parent.resolve()
+
+# Read README and requirements
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+install_requires = (here / 'requirements.txt').read_text().splitlines()
+
+
+class get_numpy_include:
+    """Defer numpy import until it is actually installed."""
+
+    def __str__(self):
+        import numpy
+
+        return numpy.get_include()
+
+
+# Define the C++ extension
 astar_module = Extension(
-    'pyastar2d.astar', sources=['src/cpp/astar.cpp', 'src/cpp/experimental_heuristics.cpp'],
+    name='pyastar2d.astar',
+    sources=[
+        'src/cpp/astar.cpp',
+        'src/cpp/experimental_heuristics.cpp',
+    ],
+    define_macros=[('Py_LIMITED_API', '0x03080000')],
+    py_limited_api=True,
     include_dirs=[
-                    numpy.get_include(),   # for numpy/arrayobject.h
-                    'src/cpp'    # for experimental_heuristics.h
-                 ],
-    extra_compile_args=["-O3", "-Wall", "-shared", "-fpic"],
+        'src/cpp',
+        get_numpy_include(),
+    ],
+    extra_compile_args=['-shared'],
+    language='c++',
 )
 
-
-with open("requirements.txt", "r") as fh:
-    install_requires = fh.readlines()
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pyastar2d",
-    version="1.0.6",
-    author="Hendrik Weideman",
-    author_email="hjweide@gmail.com",
-    description=(
-        "A simple implementation of the A* algorithm for "
-        "path-finding on a two-dimensional grid."),
+# Define package metadata
+setup(
+    name='pyastar2d',
+    version='1.1.0',
+    author='Hendrik Weideman',
+    author_email='hjweide@gmail.com',
+    description='A simple implementation of the A* algorithm for path-finding on a two-dimensional grid.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/hjweide/pyastar2d",
+    long_description_content_type='text/markdown',
+    url='https://github.com/hjweide/pyastar2d',
+    packages=find_packages(where='src', exclude=('tests',)),
+    package_dir={'': 'src'},
     install_requires=install_requires,
-    packages=setuptools.find_packages(where="src", exclude=("tests",)),
-    package_dir={"": "src"},
-    ext_modules=[astar_module],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.7',
+    ext_modules=[astar_module],
+    options={'bdist_wheel': {'py_limited_api': 'cp38'}},
 )
