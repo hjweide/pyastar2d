7d7a2d67f3d1f97e1ba62c05e4b5940413a1517d
Jason Parham, Ph.D. <bluemellophone@gmail.com>
2026-02-02 23:22:26 -0500

    Explicit Numpy 2.0 ABI (#1)
    * Explicit Numpy 2.0 ABI
    
    * Skip building for Python 3.8
    
    * Small changes
    
    * Disable Windows Arm
    
    * Bump macros, Python requires, and pyastar2d version
    
    * Bump deprecated API to Numpy 2.0

--- src/cpp/astar.cpp
+++ src/cpp/astar.cpp
@@ -2,7 +2,8 @@
 #include <limits>
 #include <cmath>
 #include <Python.h>
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define NPY_NO_DEPRECATED_API NPY_2_0_API_VERSION
+#define NPY_TARGET_VERSION NPY_2_0_API_VERSION
 #include <numpy/arrayobject.h>
 #include <iostream>
 #include <experimental_heuristics.h>
